546b6d27d07285a5f4e24ce6d472baabf1dcc8ba
wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-06-30 23:05:19 -0500

    Create the subfolder in feature

---
+++ Nasa/feature/View/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Nasa
+//
+//  Created by Wilson Alejandro Gonzalez Gaitan on 30/06/20.
+//  Copyright © 2020 Alejandro Gonzalez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+import SnapKit
+
+
+class DetailView: UIView {
+    
+}
+
